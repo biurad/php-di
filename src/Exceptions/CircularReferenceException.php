2d4fa444cd9d382a8e2d956639aed62438c97230
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-03-07 09:28:16 +0000

    Updated circular reference exception class

--- src/Exceptions/CircularReferenceException.php
+++ src/Exceptions/CircularReferenceException.php
@@ -21,4 +21,25 @@ use Psr\Container\ContainerExceptionInterface;
 
 class CircularReferenceException extends \InvalidArgumentException implements ContainerExceptionInterface
 {
+    private string $serviceId;
+
+    private array $path;
+
+    public function __construct(string $serviceId, array $path, \Throwable $previous = null)
+    {
+        parent::__construct(\sprintf('Circular reference detected for service "%s", path: "%s".', $serviceId, \implode(' -> ', $path)), 0, $previous);
+
+        $this->serviceId = $serviceId;
+        $this->path = $path;
+    }
+
+    public function getServiceId(): string
+    {
+        return $this->serviceId;
+    }
+
+    public function getPath(): array
+    {
+        return $this->path;
+    }
 }
