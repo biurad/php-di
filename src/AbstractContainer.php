7633ae0a57ccc983a1cc6e7681fcda2d1f53c7ef
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-04-05 23:48:07 +0000

    Added abstract container class

---
+++ src/AbstractContainer.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of DivineNii opensource projects.
+ *
+ * PHP version 7.4 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2021 DivineNii (https://divinenii.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Rade\DI;
+/**
+ * Internal shared container.
+ *
+ * @method call($callback, array $args = [])
+ *      Resolve a service definition, class string, invocable object or callable using autowiring.
+ * @method resolveClass(string $class, array $args = []) Resolves a class string.
+ * @method autowire(string $id, array $types) Resolve wiring classes + interfaces to service id.
+ * @method exclude(string $type) Exclude an interface or class type from being autowired.
+ *
+ * @author Divine Niiquaye Ibok <divineibok@gmail.com>
+ */
+abstract class AbstractContainer implements ContainerInterface, ResetInterface
+{
+    public const IGNORE_MULTIPLE_SERVICE = 0;
+
+    public const EXCEPTION_ON_MULTIPLE_SERVICE = 1;
+}
