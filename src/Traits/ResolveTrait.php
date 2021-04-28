0f4fe43590f655fe613b1285178e46bc2afff51a
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-04-28 15:53:12 +0000

    Improved resolving definition by it's invoke method

--- src/Traits/ResolveTrait.php
+++ src/Traits/ResolveTrait.php
@@ -68,40 +68,33 @@ trait ResolveTrait
      */
     public function __invoke()
     {
-        if (($resolved = $this->entity) instanceof Statement) {
+        $resolved = $this->entity;
+
+        if (\function_exists('trigger_deprecation') && [] !== $deprecation = $this->deprecated) {
+            \trigger_deprecation($deprecation['package'], $deprecation['version'], $deprecation['message']);
+        }
+
+        if ($resolved instanceof Statement) {
             $this->parameters += $resolved->args;
             $resolved = $resolved->value;
         }
 
         try {
             $resolved = $this->resolver->resolve($resolved, $this->resolveArguments($this->parameters, null, false));
+
+            foreach ($this->calls as $bind => $value) {
+                if (\is_object($resolved) && !\is_callable($resolved)) {
+                    $arguments = $this->resolveArguments(\is_array($value) ? $value : [$value], null, false);
+
+                    if (\property_exists($resolved, $bind)) {
+                        $resolved->{$bind} = !\is_array($value) ? \current($arguments) : $arguments;
+                    } elseif (\method_exists($resolved, $bind)) {
+                        $this->resolver->resolve([$resolved, $bind], $arguments);
+                    }
+                }
+            }
         } catch (ContainerResolutionException $e) {
-            // May be a valid object class
-            if (!\is_object($resolved)) {
-                throw $e;
-            }
-        }
-
-        if (\function_exists('trigger_deprecation') && [] !== $deprecation = $this->deprecated) {
-            \trigger_deprecation($deprecation['package'], $deprecation['version'], $deprecation['message']);
-        }
-
-        foreach ($this->calls as $bind => $value) {
-            $arguments = $this->resolveArguments(\is_array($value) ? $value : [$value], null, false);
-
-            if (\is_object($resolved) && !\is_callable($resolved)) {
-                if (\property_exists($resolved, $bind)) {
-                    $resolved->{$bind} = !\is_array($value) ? \current($arguments) : $arguments;
-
-                    continue;
-                }
-
-                if (\method_exists($resolved, $bind)) {
-                    $this->resolver->resolve([$resolved, $bind], $arguments);
-
-                    continue;
-                }
-            }
+            // No exception is need here ...
         }
 
         foreach ($this->extras as $code) {
@@ -134,7 +127,7 @@ trait ResolveTrait
         if ($entity instanceof Statement) {
             $arguments += $entity->args;
 
-            return $this->resolveEntity($entity->value, $arguments);
+            return $this->resolveEntity($entity->value, $arguments, $type);
         }
 
         if (\is_string($entity)) {
