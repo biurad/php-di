8d4f61dbf37e7e8bfffca643d9dac1ef25bd5115
Niiquaye Divine Ibok <divineibok@gmail.com>
2021-11-08 23:55:02 +0000

    Updated `\n` line break to PHP const PHP_EOL

--- src/Builder/CodePrinter.php
+++ src/Builder/CodePrinter.php
@@ -63,7 +63,7 @@ COMMENT;
             }
         }
 
-        return parent::pStmt_Property($node) . "\n";
+        return parent::pStmt_Property($node) . \PHP_EOL;
     }
 
     protected function pStmt_ClassMethod(\PhpParser\Node\Stmt\ClassMethod $node): string
@@ -75,15 +75,15 @@ COMMENT;
         }
 
         $this->indent();
-        $classMethod = \str_replace(["{\n" . ($nl = \strrev($this->nl)), $nl], ["{\n", "\n"], $classMethod);
+        $classMethod = \str_replace(["{\n" . ($nl = \strrev($this->nl)), $nl], ["{\n", \PHP_EOL], $classMethod);
         $this->outdent();
 
-        return $classMethod . "\n"; // prefer spaces instead of tab
+        return $classMethod . \PHP_EOL; // prefer spaces instead of tab
     }
 
     protected function pStmt_Class(\PhpParser\Node\Stmt\Class_ $node): string
     {
-        return \str_replace("\n" . \strrev($this->nl) . '}', "\n}", parent::pStmt_Class($node));
+        return \str_replace(\PHP_EOL . \strrev($this->nl) . '}', "\n}", parent::pStmt_Class($node));
     }
 
     protected function pScalar_String(\PhpParser\Node\Scalar\String_ $node): string
